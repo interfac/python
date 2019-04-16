366731c87c1abd7735c4d6db401bf6d7b22696ec
interfac <s.manchikatla@vikes.csuohio.edu>
2019-04-16 22:13:19 +0000

    space correction

--- 12.py
+++ 12.py
@@ -3,12 +3,12 @@ def add(a,b):
 def sub(a,b):
  print "The addition is ",a-b
 
-print("Enter 1 for add/n")
-print("Enter 2 for sub/n")
+print("Enter 1 for add\n")
+print("Enter 2 for sub\n")
 print("Choose")
-x=input("Now/n")
-a=input("Enter the 1st number/n")
-b=input("Enter the 2nd number/n")
+x=input("Now\n")
+a=input("Enter the 1st number\n")
+b=input("Enter the 2nd number\n")
 if x==1:
  add(a,b)
 else:
