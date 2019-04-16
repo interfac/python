5a1c95dbfbc1b05136a2eba22cfe3be102c30a25
interfac <s.manchikatla@vikes.csuohio.edu>
2019-04-16 22:36:01 +0000

    */

--- 12.py
+++ 12.py
@@ -1,14 +1,34 @@
-def add(a,b):
- print "The addition is ",a+b
-def sub(a,b):
- print "The subtraction is ",a-b
+def in1():
+ a=input("Enter the 1st number")
+ b=input("Enter the 2nd number")
+ return a,b
+def add():
+ c, d=in1()
+ print "The addition is ",c+d
+def sub():
+ c, d=in1()
+ print "The subtraction is ",c-d
+def mul():
+ c, d=in1()
+ print "The multiplication is ",c*d
+def div():
+ c, d=in1()
+ print "The division is ",c/d
 
-print("Enter 1 for add")
-print("Enter 2 for sub")
-x=input("Choose Now\n")
-a=input("Enter the 1st number")
-b=input("Enter the 2nd number")
-if x==1:
- add(a,b)
-else:
- sub(a,b)
+while True:
+ print("Enter 1 for add")
+ print("Enter 2 for sub")
+ print("Enter 3 for mul")
+ print("Enter 4 for div")
+ x=input("Choose Now\n")
+ if x==1:
+  add()
+ elif x==2:
+  sub()
+ elif x==3:
+  mul()
+ elif x==4:
+  div()
+ else:
+  print("Wrong Choice")
+  break
