ceb5790f88b5c4b093224da8bd4afeccaf03e130
interfac <interfac@users.noreply.github.com>
2019-04-16 18:11:12 -0400

    Update 12.py
    spaces

--- 12.py
+++ 12.py
@@ -1,14 +1,14 @@
 def add(a,b):
- print("The addition is ",a+b)
+ print "The addition is ",a+b
 def sub(a,b):
- print("The addition is ",a-b)
+ print "The addition is ",a-b
 
-print("Enter 1 for add")
-print("Enter 2 for sub")
+print("Enter 1 for add/n")
+print("Enter 2 for sub/n")
 print("Choose")
-x=input("Now")
-a=input("Enter the 1st number")
-b=input("Enter the 2nd number")
+x=input("Now/n")
+a=input("Enter the 1st number/n")
+b=input("Enter the 2nd number/n")
 if x==1:
  add(a,b)
 else:
